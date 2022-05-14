c862b661ac42cfa67dba639ec7d0d9e8aad2e78d
Md. Maruf Rien <maruf14222@gmail.com>
2022-05-14 14:48:54 +0600

    feat: Add nav

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,15 @@
+import Nav from "./Nav";
 import styles from "../styles/Layout.module.css"
 const Layout = ({ children }) => {
 	return (
-		<div className={styles.container}>
-			<main className={styles.main}>
-				{children}
-			</main>
+		<div>
+			<Nav />
+			<div className={styles.container}>
+				<main className={styles.main}>
+					{children}
+				</main>
+			</div>
 		</div>
-
 	)
 }
 
